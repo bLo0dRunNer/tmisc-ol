bbedfe403fbf47e4d2e637d5ae6ddd37227ffb22
bLo0dRunNer <bloodrunnermf@hotmail.com>
2013-10-11 15:49:59 +0200

    minor output adjustment

--- app-laptop/samsung-fn-keys/files/samsung_fn_keys.sh
+++ app-laptop/samsung-fn-keys/files/samsung_fn_keys.sh
@@ -1,36 +1,40 @@
 #!/bin/bash
 
 echo ""
-echo "* Piping Force Key Release:"
-echo "* Key Event Codes for FN-Keys: 130,131,132,134,136,137,179,247,249 to: /sys/devices/platform/i8042/serio0/force_release"
+echo ""
+echo " * Piping Force Key Release:"
+echo " * Key Event Codes for FN-Keys: 130,131,132,134,136,137,179,247,249 to: /sys/devices/platform/i8042/serio0/force_release"
 echo 130,131,132,134,136,137,179,247,249 > /sys/devices/platform/i8042/serio0/force_release
+echo ""
 
 setkeycodes e002 227
-echo "* setkeycodes e002 227 # Fn+F4 maps to switchvidmode"
+echo " * setkeycodes e002 227 # Fn+F4 maps to switchvidmode"
 
 setkeycodes e003 236
-echo "* setkeycodes e003 236 # Fn+F2 maps to battery"
+echo " * setkeycodes e003 236 # Fn+F2 maps to battery"
 
 setkeycodes e004 148
-echo "* setkeycodes e004 148 # Fn+F5 maps to prog1"
+echo " * setkeycodes e004 148 # Fn+F5 maps to prog1"
 
 setkeycodes e006 238
-echo "* setkeycodes e006 238 # Fn+F9 maps to wlan"
+echo " * setkeycodes e006 238 # Fn+F9 maps to wlan"
 
 setkeycodes e008 225
-echo "* setkeycodes e008 225 # Fn+Up maps to brightnessup"
+echo " * setkeycodes e008 225 # Fn+Up maps to brightnessup"
 
 setkeycodes e009 224
-echo "* setkeycodes e009 224 # Fn+Dn maps to brightnessdown"
+echo " * setkeycodes e009 224 # Fn+Dn maps to brightnessdown"
 
 setkeycodes e031 149
-echo "* setkeycodes e031 149 # Fn+F7 maps to prog2"
+echo " * setkeycodes e031 149 # Fn+F7 maps to prog2"
 
 setkeycodes e033 202
-echo "* setkeycodes e033 202 # Fn+F8 maps to prog3"
+echo " * setkeycodes e033 202 # Fn+F8 maps to prog3"
 
 setkeycodes e077 191
-echo "* setkeycodes e077 191 # Fn+F10 maps to F21 whenever the touchpad is enabled"
+echo " * setkeycodes e077 191 # Fn+F10 maps to F21 whenever the touchpad is enabled"
 
 setkeycodes e079 192
-echo "* setkeycodes e079 192 # Fn+F10 maps to F22 whenever the touchpad is disabled"
+echo " * setkeycodes e079 192 # Fn+F10 maps to F22 whenever the touchpad is disabled"
+
+echo ""
